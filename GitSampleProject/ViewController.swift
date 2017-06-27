137aac5c882079e39d950d189836738dbce7c584
MallikaDevi <vishwaksolutions@vs-mallikadevib.local>
2017-06-27 13:52:08 +0530

    committed by mallika with addition method

--- GitSampleProject/ViewController.swift
+++ GitSampleProject/ViewController.swift
@@ -20,10 +20,21 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         print("Welcome To Vishwak 2017")
     }
+    override func viewWillLayoutSubviews() {
+        self.addingTwoMembers()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    func addingTwoMembers() {
+        var a = Int()
+        var b = Int()
+        a = 10
+        b = 12
+        let c = a + b
+        print(c)
+ 
+    }
 
 }
 
